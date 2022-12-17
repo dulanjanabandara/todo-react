1de00d4ae772f5259d903960248f31c64420e653
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-17 13:01:50 +0530

    Created create task component and title, description fields of the create task component.

---
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -0,0 +1,26 @@
+import React, { FC, ReactElement } from 'react';
+import { Box, Typography, Stack } from '@mui/material';
+
+import { TaskTitleField } from './_tastTitleField';
+import { TaskDescriptionField } from './_taskDescriptionField';
+
+export const CreateTaskForm: FC = (): ReactElement => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="flex-start"
+      width="100%"
+      px={4}
+      my={6}
+    >
+      <Typography mb={2} component="h2" variant="h6">
+        Create a Task
+      </Typography>
+      <Stack sx={{ width: '100%' }} spacing={2}>
+        <TaskTitleField />
+        <TaskDescriptionField />
+      </Stack>
+    </Box>
+  );
+};
