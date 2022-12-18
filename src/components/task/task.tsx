0faeb904df53242fe715b8c1d82238ead051acba
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-18 07:01:58 +0530

    Completed creating task component by adding prop types and type checking to the whole component.

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,11 +1,26 @@
 import React, { FC, ReactElement } from 'react';
 import { Box } from '@mui/material';
+import PropTypes from 'prop-types';
 
 import { TaskHeader } from './_taskHeader';
 import { TaskDescription } from './_taskDescription';
 import { TaskFooter } from './_taskFooter';
+import { ITask } from './interfaces/ITask';
+import { Status } from '../createTaskForm/enums/Status';
+import { Priority } from '../createTaskForm/enums/Priority';
+import { renderPriorityBorderColor } from './helpers/renderPriorityBorderColor';
 
-export const Task: FC = (): ReactElement => {
+export const Task: FC<ITask> = (props): ReactElement => {
+  const {
+    title = 'Test Title',
+    date = new Date(),
+    description = 'Test Description',
+    priority = Priority.normal,
+    status = Status.completed,
+    onStatusChange = (e) => console.log(e),
+    onClick = (e) => console.log(e),
+  } = props;
+
   return (
     <Box
       display="flex"
@@ -19,12 +34,22 @@ export const Task: FC = (): ReactElement => {
         backgroundColor: 'background.paper',
         borderRadius: '8px',
         border: '1px solid',
-        borderColor: 'error.light',
+        borderColor: renderPriorityBorderColor(priority),
       }}
     >
-      <TaskHeader />
-      <TaskDescription />
-      <TaskFooter />
+      <TaskHeader title={title} date={date} />
+      <TaskDescription description={description} />
+      <TaskFooter onClick={onClick} onStatusChange={onStatusChange} />
     </Box>
   );
+};
+
+Task.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.instanceOf(Date),
+  description: PropTypes.string,
+  priority: PropTypes.string,
+  status: PropTypes.string,
+  onClick: PropTypes.func,
+  onStatusChange: PropTypes.func,
 };
