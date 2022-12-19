6afa4c363a67a304c44017667f900e4a0164db9a
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-19 17:02:37 +0530

    Implemented the task getting request.

---
+++ src/components/taskArea/interfaces/ITaskApi.ts
@@ -0,0 +1,13 @@
+import { Priority } from "../../createTaskForm/enums/Priority";
+import { Status } from "../../createTaskForm/enums/Status";
+
+
+export interface ITaskApi {
+    id: string;
+    date: string;
+    title:string;
+    description:string;
+    status: `${Status}`;
+    priority: `${Priority}`
+}
+
--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -1,11 +1,21 @@
 import React, { FC, ReactElement } from 'react';
 import { Grid, Box } from '@mui/material';
 import { format } from 'date-fns';
+import { useQuery } from '@tanstack/react-query';
 
 import { TaskCounter } from '../taskCounter/taskCounter';
 import { Task } from '../task/task';
+import { sendApiRequest } from '../../helpers/sendApiRequest';
+import { ITaskApi } from './interfaces/ITaskApi';
 
 export const TaskArea: FC = (): ReactElement => {
+  const { error, isLoading, data, refetch } = useQuery(['tasks'], async () => {
+    return await sendApiRequest<ITaskApi[]>(
+      'http://localhost:3200/tasks',
+      'GET',
+    );
+  });
+
   return (
     <Grid item md={8} px={4}>
       <Box mb={8} px={4}>
