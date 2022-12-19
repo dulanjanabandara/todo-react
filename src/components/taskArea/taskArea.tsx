f6eda55fbebbaa9d9b6210cd1cec5cbe3148c289
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-19 18:00:55 +0530

    Modified task component to display task data from the state.

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactElement } from 'react';
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, Alert, LinearProgress } from '@mui/material';
 import { format } from 'date-fns';
 import { useQuery } from '@tanstack/react-query';
 
@@ -37,8 +37,36 @@ export const TaskArea: FC = (): ReactElement => {
           <TaskCounter />
         </Grid>
         <Grid item display="flex" flexDirection="column" xs={10} md={8}>
-          <Task />
-          <Task />
+          <>
+            {error && (
+              <Alert severity="error">
+                There was an error fetching your data!
+              </Alert>
+            )}
+            {!error && Array.isArray(data) && data.length === 0 && (
+              <Alert severity="warning">
+                You do not have any tasks yet. Start by creating some tasks.
+              </Alert>
+            )}
+
+            {isLoading ? (
+              <LinearProgress />
+            ) : (
+              Array.isArray(data) &&
+              data.length > 0 &&
+              data.map((dataItem, index) => (
+                <Task
+                  key={index + dataItem.priority}
+                  id={dataItem.id}
+                  title={dataItem.title}
+                  date={new Date(dataItem.date)}
+                  description={dataItem.description}
+                  status={dataItem.status}
+                  priority={dataItem.priority}
+                />
+              ))
+            )}
+          </>
         </Grid>
       </Grid>
     </Grid>
