503c275a2a9f7457f5856873c5d3c37b590d1c5e
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-19 10:30:04 +0530

    Integrated react query(tanstack query) with the frontend.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React, { FC, ReactElement } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { customTheme } from './themes/customTheme';
 import { Dashboard } from './pages/dashboard/dashboard';
 
+const queryClient = new QueryClient();
+
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
