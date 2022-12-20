fd8772a03af3843f3c296f86ec2a40d862e7248a
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-20 07:11:05 +0530

    Implemented counters functionality.

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -9,6 +9,7 @@ import { sendApiRequest } from '../../helpers/sendApiRequest';
 import { ITaskApi } from './interfaces/ITaskApi';
 import { Status } from '../createTaskForm/enums/Status';
 import { IUpdateTask } from '../createTaskForm/interfaces/IUpdateTask';
+import { countTasks } from './helpers/countTasks';
 
 export const TaskArea: FC = (): ReactElement => {
   const { error, isLoading, data, refetch } = useQuery(['tasks'], async () => {
@@ -57,9 +58,18 @@ export const TaskArea: FC = (): ReactElement => {
           xs={12}
           mb={8}
         >
-          <TaskCounter />
-          <TaskCounter />
-          <TaskCounter />
+          <TaskCounter
+            status={Status.todo}
+            count={data ? countTasks(data, Status.todo) : undefined}
+          />
+          <TaskCounter
+            status={Status.inProgress}
+            count={data ? countTasks(data, Status.inProgress) : undefined}
+          />
+          <TaskCounter
+            status={Status.completed}
+            count={data ? countTasks(data, Status.completed) : undefined}
+          />
         </Grid>
         <Grid item display="flex" flexDirection="column" xs={10} md={8}>
           <>
