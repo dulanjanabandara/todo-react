f2044b50ca177430d10c595f4f4afe297938d7c9
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-20 07:37:56 +0530

    Created react context and integrated with the app.

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { customTheme } from './themes/customTheme';
 import { Dashboard } from './pages/dashboard/dashboard';
+import ComposeContext from './context/Compose.context';
+import { rootContext } from './context/root.context';
 
 const queryClient = new QueryClient();
 
 const App: FC = (): ReactElement => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={customTheme}>
-        <CssBaseline />
-        <Dashboard />
-      </ThemeProvider>
+      <ComposeContext components={rootContext}>
+        <ThemeProvider theme={customTheme}>
+          <CssBaseline />
+          <Dashboard />
+        </ThemeProvider>
+      </ComposeContext>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
